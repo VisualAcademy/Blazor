3203445357e8f358b7e151c32d5802f26473105a
RedPlus <redplus@redplus.net>
2019-10-30 07:25:17 +0900

    웹앱, 클래스라이브러리, SQL서버데이터베이스프로젝트 추가

---
+++ CourseApp.Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseApp.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
---
+++ CourseApp.SqlServer/Courses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Courses]
+(
+	[CourseId] INT NOT NULL PRIMARY KEY Identity(1, 1), -- 일련번호
+	[CourseName] NVarChar(100) Not Null					-- 과목이름
+);
+
